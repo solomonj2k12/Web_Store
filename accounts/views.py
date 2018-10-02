b1de2ef1dfbe5a4990a683a854de2b06dcd5b37c
sol_rosser <solomonj2k12@hotmail.co.uk>
2018-10-02 10:13:50 +0000

    first commit, have so far added the ability to logout from account, tested using superuser

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+
+
+def index(request):
+    '''Return the index.html file '''
+    return render(request, 'index.html')
+    
+def logout(request):
+    ''' log the user out '''
+    auth.logout(request)
+    messages.success(request, "You Have Successfully Logged Out!")
+    return redirect(reverse('index'))
